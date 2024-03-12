7e5afaaaeaf108c13894c25a76ee16608c79b919
passkera <passkera@gmail.com>
2024-03-12 19:14:22 +0300

    Task 9

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,13 @@
 import React from 'react';
 
 function App() {
-  return <div>
-  <h1 class='paragraph1'>React App</h1>
-  <h3 class='paragraph2'>Readme</h3>
-
+  return (
   <ul>
     <li>1</li>
     <li>2</li>
     <li>3</li>
-    <li>4</li>
-    <li>5</li>
-    <li>6</li>
-    <li>7</li>
-    <li>8</li>
-    <li>9</li>
-    <li>10</li>
   </ul>
-  </div>
+  )
 }
 
 export default App;
