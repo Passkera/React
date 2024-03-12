f9133e8dbaa3a22c5fb17d9912196d9db637c1f9
passkera <passkera@gmail.com>
2024-03-12 19:34:05 +0300

    Task 23

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,11 +3,40 @@ import React from 'react';
 function App() {
 	
 	return <div>
-		<input />
-		<br />
-		<input />
-		<br />
-		<input />
+		<ul>
+			<li>1</li>
+			<li>2</li>
+			<li>3</li>
+			<li>4</li>
+			<li>5</li>
+			<li>6</li>
+			<li>7</li>
+			<li>8</li>
+			<li>9</li>
+			<li>10</li>
+		</ul>
+
+		<table>
+			<thead>
+				<tr>
+					<td>1</td>
+					<td>2</td>
+					<td>3</td>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<td>01</td>
+					<td>02</td>
+					<td>03</td>
+				</tr>
+				<tr>
+					<td>11</td>
+					<td>12</td>
+					<td>13</td>
+				</tr>
+			</tbody>
+		</table>
 	</div>
 }
 
