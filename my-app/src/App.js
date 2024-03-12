6bf6f59c18dc27e8babe9f15d848fe0533d966fe
passkera <passkera@gmail.com>
2024-03-12 19:29:01 +0300

    Task 22

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 
 function App() {
-	const items = <>
-	<li>text1</li>
-	<li>text2</li>
-	<li>text3</li></>;
 	
-	return <ul>
-		{items}
-	</ul>
+	return <div>
+		<input />
+		<br />
+		<input />
+		<br />
+		<input />
+	</div>
 }
 
 export default App;
