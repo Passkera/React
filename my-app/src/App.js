e0620c76c72879a629600326fc2452b4fa768096
passkera <passkera@gmail.com>
2024-03-12 20:24:02 +0300

    Task 26

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,43 +1,20 @@
 import React from 'react';
 
 function App() {
+
+	let isAdult = false
+	let text
+
+	if (isAdult) {
+		text = <p>A user is a adult</p>
+	} else {
+		text = <p>A user isn't a adult</p>	
+	}
 	
 	return <div>
-		<ul>
-			<li>1</li>
-			<li>2</li>
-			<li>3</li>
-			<li>4</li>
-			<li>5</li>
-			<li>6</li>
-			<li>7</li>
-			<li>8</li>
-			<li>9</li>
-			<li>10</li>
-		</ul>
-
-		<table>
-			<thead>
-				<tr>
-					<td>1</td>
-					<td>2</td>
-					<td>3</td>
-				</tr>
-			</thead>
-			<tbody>
-				<tr>
-					<td>01</td>
-					<td>02</td>
-					<td>03</td>
-				</tr>
-				<tr>
-					<td>11</td>
-					<td>12</td>
-					<td>13</td>
-				</tr>
-			</tbody>
-		</table>
+		{text}
 	</div>
+		
 }
 
 export default App;
