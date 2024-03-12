f8e625630fc941367c88c4ddb1a8606d9b6c0e29
passkera <passkera@gmail.com>
2024-03-12 19:19:25 +0300

    Task 15

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 
 function App() {
-	const name = 'user';
-	const age  = '30';
-	
-	return <div>
-		name: {name}<br></br>
-		age:  {age}
-	</div>;
+	const arr = [1, 2, 3, 4, 5];
+
+	return <ul>
+		<li>{arr[0]}</li>
+		<li>{arr[1]}</li>
+		<li>{arr[2]}</li>
+		<li>{arr[3]}</li>
+		<li>{arr[4]}</li>
+	</ul>
 }
 
 export default App;
