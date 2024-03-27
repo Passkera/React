198868660b8225c17dfcd767e76f1c4665d4144c
passkera <passkera@gmail.com>
2024-03-27 08:14:38 +0300

    Task 33

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,22 +1,18 @@
 import React from 'react';
 
 function App() {
-
-	function GetDigitSum(val){
-		let result = 0;
-
-		for(let i = 0; i < String(val).length; i++){
-			console.log(String(val)[i])
-			result += Number(String(val)[i])
-		}
-			
-		return result;
+	function show1() {
+		alert(1);
+	}
+	
+	function show2() {
+		alert(2);
 	}
 	
 	return <div>
-		<p>{GetDigitSum(12345)}</p>
-	</div>
-		
+		<button onClick={show2}>act1</button>
+		<button onClick={show1}>act2</button>
+	</div>;
 }
 
 export default App;
